25d4261976495592312d60ea008457d355fe7bc0
Mateo Cereceda <70116950+MateoCereceda@users.noreply.github.com>
2023-05-06 18:13:32 -0400

    Funcionando FireBase
    Funciona! Se guarda la información de contacto proporcionada en una base de datos.

--- restaurante/src/components/ContactForm.js
+++ restaurante/src/components/ContactForm.js
@@ -1,55 +1,77 @@
-import React, { useState } from 'react';
-import './styles/ContactForm.css';
+import React, { useState } from "react";
+import "./styles/ContactForm.css";
+import db from "./firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const ContactForm = () => {
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [phone, setPhone] = useState('');
-const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
-const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-};
 
-return (
+    try {
+      const docRef = await addDoc(collection(db, "contactMessages"), {
+        name,
+        email,
+        phone,
+        message,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      // Aquí puedes agregar la animación de confirmación de envío
+      
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+    // Restablece los valores de los campos del formulario
+    setName("");
+    setEmail("");
+    setPhone("");
+    setMessage("");
+  };
+
+  return (
     <div className="contact-form">
-    <h2>Formulario de contacto</h2>
-    <form onSubmit={handleSubmit}>
+      <h2>Formulario de contacto</h2>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Nombre:</label>
         <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
 
         <label htmlFor="email">Correo electrónico:</label>
         <input
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <label htmlFor="phone">Teléfono:</label>
         <input
-        type="tel"
-        id="phone"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
         />
 
         <label htmlFor="message">Mensaje:</label>
         <textarea
-        id="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
 
         <button type="submit">Enviar</button>
-    </form>
+      </form>
     </div>
-);
+  );
 };
 
 export default ContactForm;
